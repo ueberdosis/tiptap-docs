b67abc19d468bc957062f3abece8e47708d7550a
Martin Kriegeskorte <martin.kriegeskorte@ueber.io>
2024-07-02 09:46:43 +0200

    chore: make canonical generation more robust

--- src/server/createCanonicalPath.ts
+++ src/server/createCanonicalPath.ts
@@ -1,6 +1,9 @@
-import { FULL_DOMAIN } from '@/utils/constants'
+import { DOMAIN, BASE_PATH } from '@/utils/constants'
 
 export const createCanonicalPath = (path: string[]) => {
-  const canonicalPath = `${FULL_DOMAIN}/${path.join('/')}`
-  return canonicalPath
+  const urlPath = [BASE_PATH, ...path].join('/')
+  const canonicalUrl = new URL(urlPath, DOMAIN)
+  const href = canonicalUrl.href
+
+  return href
 }
