bfce0d58417f32c2baacda92b5f203ae27330c82
ThisDavidRichard <147505039+ThisDavidRichard@users.noreply.github.com>
2024-07-14 18:04:55 +0200

    Fixed spelling in sidebar

--- src/content/examples/sidebar.ts
+++ src/content/examples/sidebar.ts
@@ -81,7 +81,7 @@ export const sidebarConfig: SidebarConfig = {
           href: '/examples/advanced/interactive-react-and-vue-views',
         },
         {
-          title: 'React Performance',
+          title: 'React performance',
           href: '/examples/advanced/react-performance',
         },
         {
