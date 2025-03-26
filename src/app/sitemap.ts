e6a45368a5357857775eca82fe7894b1edfe54e4
Dominik Biedebach <dominik.biedebach@ueber.io>
2025-03-26 11:51:05 +0100

    update sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,26 @@
 import { MetadataRoute } from 'next'
 import { getAllContentPaths } from '@/server/getAllContentPaths'
-import { FULL_DOMAIN } from '@/utils/constants'
+import { FULL_DOMAIN, VERSIONS } from '@/utils/constants'
+import { getRecentVersion } from '@/utils/versioning'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const paths = await getAllContentPaths({ withoutIndex: true })
+  const recentVersion = getRecentVersion(VERSIONS)
 
-  const pathEntries = paths.map((path) => ({
-    url: `${FULL_DOMAIN}${path}`,
-    lastModified: new Date(),
-    changeFrequency: 'monthly',
-    priority: 0.7,
-  })) as MetadataRoute.Sitemap
+  const pathEntries = paths.map((path) => {
+    const isRecentVersion = recentVersion ? path.includes(recentVersion.version) : false
+
+    if (isRecentVersion && recentVersion) {
+      path = path.replace(`${recentVersion.version}/`, '')
+    }
+
+    return {
+      url: `${FULL_DOMAIN}${path}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    }
+  }) as MetadataRoute.Sitemap
 
   return [
     // HOME ROUTE
--- src/utils/versioning.ts
+++ src/utils/versioning.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import type { PageFrontmatter } from '@/types'
+import type { ExternalVersion, InternalVersion, VersionData } from '@/types'
 
 export const getImportPathForVersion = (version: string, ...incPath: string[]) =>
   path.join(process.cwd(), 'src/content', version, ...incPath)
@@ -9,4 +10,39 @@ export const importMdxFromPath = async (version: string, incPath: string) => {
     default: () => JSX.Element
     frontmatter?: PageFrontmatter
   }
+}
+
+/**
+ * Find the most recent version that is not a beta version
+ * @param versions The list of versions to search
+ */
+export const getRecentVersion = (versions: Array<VersionData>) => {
+  return versions.filter((version) => !version.isBeta).at(-1)
+}
+
+/**
+ * Finds the most recent beta version
+ * @param versions The list of versions to search
+ * @returns The most recent beta version
+ */
+export const getRecentBetaVersion = (versions: Array<VersionData>) => {
+  return versions.filter((version) => version.isBeta).at(-1)
+}
+
+/**
+ * Typeguard to check if a version is external by checking if it has a url
+ * @param version The version to check
+ * @returns boolean
+ */
+export const isExternalVersion = (version: VersionData): version is ExternalVersion => {
+  return !!(version as ExternalVersion).url
+}
+
+/**
+ * Typeguard to check if a version is internal by checking if it has a key
+ * @param version The version to check
+ * @returns boolean
+ */
+export const isInternalVersion = (version: VersionData): version is InternalVersion => {
+  return !!(version as InternalVersion).key
 }
