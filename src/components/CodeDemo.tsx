626512895849b1a6544103955b2d0027784fb4ff
ThisDavidRichard <147505039+ThisDavidRichard@users.noreply.github.com>
2025-04-09 11:50:04 +0200

    added scrollable functionality to codeDemo component

--- src/components/CodeDemo.tsx
+++ src/components/CodeDemo.tsx
@@ -4,19 +4,21 @@ import IframeResizer from 'iframe-resizer-react'
 import { DEMO_URL, PRO_DEMO_URL } from '@/utils/constants'
 
 export type CodeDemoProps = {
-  src?: string
-  path: string
-  isPro?: boolean
-  isSmall?: boolean
+    src?: string
+    path: string
+    isPro?: boolean
+    isSmall?: boolean
+    isScrollable?: boolean
 }
 
-export const CodeDemo = ({ src, path, isPro, isSmall }: CodeDemoProps) => {
-  return (
-    <div className="shadow-cardHover rounded-[0.625rem] my-12 bg-white">
-      <IframeResizer
-        src={src ? src : (isPro ? PRO_DEMO_URL : DEMO_URL) + path}
-        className={`w-full ${isSmall ? 'h-60' : 'h-96'}`}
-      />
-    </div>
-  )
+export const CodeDemo = ({ src, path, isPro, isSmall, isScrollable = false }: CodeDemoProps) => {
+    return (
+        <div className="shadow-cardHover rounded-[0.625rem] my-12 bg-white">
+            <IframeResizer
+                src={src ? src : (isPro ? PRO_DEMO_URL : DEMO_URL) + path}
+                className={`w-full ${isSmall ? 'h-60' : 'h-96'}`}
+                scrolling={isScrollable}
+            />
+        </div>
+    )
 }
