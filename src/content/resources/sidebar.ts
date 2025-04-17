8ced08f7fe5fb78ca2399ed5dc50ec4c59e6ce1c
ThisDavidRichard <147505039+ThisDavidRichard@users.noreply.github.com>
2025-04-17 12:15:13 +0200

    Added main sidebar to resources

---
+++ src/content/resources/sidebar.ts
@@ -0,0 +1,86 @@
+import { SidebarConfig } from '@/types'
+
+export const sidebarConfig: SidebarConfig = {
+  id: 'all-docs',
+  rootHref: '/',
+  title: 'Home',
+  items: [
+    {
+      type: 'group',
+      title: 'Getting started',
+      href: '/getting-started',
+      children: [
+        {
+          href: '/',
+          title: 'Overview',
+        },
+        {
+          href: '/resources/whats-new',
+          title: "What's new in V3",
+        },
+      ],
+    },
+    {
+      type: 'group',
+      title: 'Browse by feature',
+      href: '/suite-docs',
+      children: [
+        {
+          href: '/editor/getting-started/overview',
+          title: 'Editor',
+        },
+        {
+          href: '/collaboration/getting-started/overview',
+          title: 'Collaboration',
+        },
+        {
+          href: '/comments/getting-started/overview',
+          title: 'Comments',
+        },
+        {
+          href: '/content-ai/getting-started/overview',
+          title: 'Content AI',
+        },
+        {
+          href: '/collaboration/documents/history',
+          title: 'History',
+        },
+        {
+          href: '/conversion/getting-started/overview',
+          title: 'Conversion',
+        },
+        {
+          href: '/collaboration/documents/semantic-search',
+          title: 'Semantic Search',
+        },
+      ],
+    },
+    {
+      type: 'group',
+      title: 'Resources',
+      href: '/',
+      children: [
+        {
+          href: '/guides',
+          title: 'Guides',
+        },
+        {
+          href: '/examples',
+          title: 'Examples',
+        },
+        {
+          href: '/resources/contributing',
+          title: 'Contributing',
+        },
+        {
+          href: '/resources/changelog',
+          title: 'Editor changelog',
+        },
+        {
+          href: '/resources/pro-license',
+          title: 'Pro license',
+        },
+      ],
+    },
+  ],
+}
