82b6a11a50f4fefc01cd5541311c95e2f4fa2cdb
bdbch <6538827+bdbch@users.noreply.github.com>
2025-05-05 16:46:54 +0200

    New version picker for stable (#151)
    * Feature/version switch (#149)
    
    * added version switch
    
    * added changeset
    
    * added version picker for stable
    
    * refactored version picker (#150)
    
    * update version switch

---
+++ src/components/VersionSwitch.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { ChevronDownIcon } from 'lucide-react'
+import { NavLinkButton } from './ui/NavLinkButton'
+import { NavLink } from './NavLink'
+import { Tag } from './ui/Tag'
+import { VERSIONS, CURRENT_VERSION } from '@/utils/constants'
+import type { VersionData } from '@/types'
+
+const getItemLabel = ({ version }: { version: VersionData }) => {
+  if (version.isLegacy) return 'Legacy'
+  if (version.isBeta) return 'Beta'
+  if (version.isAlpha) return 'Alpha'
+  if (version.isRc) return 'RC'
+  return false
+}
+
+const VersionItem = ({ version }: { version: VersionData }) => {
+  const label = getItemLabel({ version })
+  const isCurrentVersion = version.version === CURRENT_VERSION
+
+  return (
+    <DropdownMenu.Item asChild>
+      <NavLink href={version.url} target={!isCurrentVersion ? '_blank' : ''} hideIcon>
+        {version.version}
+        {label ? <Tag className="ml-1">{label}</Tag> : null}
+      </NavLink>
+    </DropdownMenu.Item>
+  )
+}
+
+export const VersionSwitch = () => {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <NavLinkButton className="p-1 text-base">
+          {CURRENT_VERSION}
+          <ChevronDownIcon className="w-3 h-3" />
+        </NavLinkButton>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="z-50 flex flex-col gap-6 px-2 py-6 text-black bg-white border rounded-lg shadow-md lg:py-3 border-grayAlpha-100">
+          <div>
+            {VERSIONS.map((version) => (
+              <VersionItem key={version.version} version={version} />
+            ))}
+          </div>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
