f76ff692891e61952048545afd956183156b88f1
bdbch <dominik@bdbch.com>
2025-05-09 09:51:07 +0200

    don't generate canonical on next envs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,6 +8,7 @@ export const PRO_DEMO_URL =
 export const BASE_PATH = process.env.BASE_PATH || process.env.NEXT_PUBLIC_BASE_PATH || ''
 export const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000'
 export const FULL_DOMAIN = `${DOMAIN}${BASE_PATH}`
+export const IS_NEXT = process.env.NEXT_PUBLIC_NEXT === 'true' ? true : false
 
 export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || null
 export const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT || 'development'
