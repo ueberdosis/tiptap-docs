e6ef9a73758931f3c18de895b9381d59bbc816c9
Jan Thurau <jan@janthurau.de>
2025-06-12 20:00:39 +0200

    wip

--- src/content/hocuspocus/sidebar.ts
+++ src/content/hocuspocus/sidebar.ts
@@ -28,5 +28,137 @@ export const sidebarConfig: SidebarConfig = {
         },
       ],
     },
+    {
+      type: 'group',
+      href: '/hocuspocus/provider',
+      title: 'Provider',
+      children: [
+        {
+          title: 'Overview',
+          href: '/hocuspocus/provider/overview',
+        },
+        {
+          title: 'Install',
+          href: '/hocuspocus/provider/install',
+        },
+        {
+          title: 'Configuration',
+          href: '/hocuspocus/provider/configuration',
+        },
+        {
+          title: 'Events',
+          href: '/hocuspocus/provider/events',
+        },
+        {
+          title: 'Examples',
+          href: '/hocuspocus/provider/examples',
+        },
+      ],
+    },
+    {
+      type: 'group',
+      href: '/hocuspocus/server',
+      title: 'Server',
+      children: [
+        {
+          title: 'Overview',
+          href: '/hocuspocus/server/overview',
+        },
+        {
+          title: 'Install',
+          href: '/hocuspocus/server/install',
+        },
+        {
+          title: 'Configuration',
+          href: '/hocuspocus/server/configuration',
+        },
+        {
+          title: 'Hooks',
+          href: '/hocuspocus/server/hooks',
+        },
+        {
+          title: 'Usage',
+          href: '/hocuspocus/server/usage',
+        },
+        {
+          title: 'Examples',
+          href: '/hocuspocus/server/examples',
+        },
+      ],
+    },
+    {
+      type: 'group',
+      href: '/hocuspocus/server/extensions',
+      title: 'Server Extensions',
+      children: [
+        {
+          title: 'Overview',
+          href: '/hocuspocus/server/extensions/overview',
+        },
+        {
+          title: 'Database',
+          href: '/hocuspocus/server/extensions/database',
+        },
+        {
+          title: 'Logger',
+          href: '/hocuspocus/server/extensions/logger',
+        },
+        {
+          title: 'Redis',
+          href: '/hocuspocus/server/extensions/redis',
+        },
+        {
+          title: 'SQLite',
+          href: '/hocuspocus/server/extensions/sqlite',
+        },
+        {
+          title: 'Throttle',
+          href: '/hocuspocus/server/extensions/throttle',
+        },
+        {
+          title: 'Webhook',
+          href: '/hocuspocus/server/extensions/webhook',
+        },
+      ],
+    },
+    {
+      type: 'group',
+      href: '/hocuspocus/guides',
+      title: 'Guides',
+      children: [
+        {
+          title: 'Overview',
+          href: '/hocuspocus/guides/overview',
+        },
+        {
+          title: 'Authentication',
+          href: '/hocuspocus/guides/authentication',
+        },
+        {
+          title: 'Persistence',
+          href: '/hocuspocus/guides/persistence',
+        },
+        {
+          title: 'Collaborative Editing',
+          href: '/hocuspocus/guides/collaborative-editing',
+        },
+        {
+          title: 'Custom Extensions',
+          href: '/hocuspocus/guides/custom-extensions',
+        },
+        {
+          title: 'Awareness',
+          href: '/hocuspocus/guides/awareness',
+        },
+        {
+          title: 'Multi Subdocuments',
+          href: '/hocuspocus/guides/multi-subdocuments',
+        },
+        {
+          title: 'Scalability',
+          href: '/hocuspocus/guides/scalability',
+        },
+      ],
+    },
   ],
 }
