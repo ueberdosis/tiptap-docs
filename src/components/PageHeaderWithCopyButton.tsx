e6422332e1c1a4caa2f581621c137c322ecfd7ef
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2025-10-15 13:35:33 +0200

    feat: add documentation comments

--- src/components/PageHeaderWithCopyButton.tsx
+++ src/components/PageHeaderWithCopyButton.tsx
@@ -4,11 +4,25 @@ import { SidebarConfig } from '@/types'
 import { cn } from '@/utils'
 
 export type PageHeaderWithCopyButtonProps = {
+  /**
+   * Configuration for the sidebar. Used by the PageHeaderBreadcrumbs component.
+   */
   config: SidebarConfig
+
+  /**
+   * Path to the markdown file. Used by the CopyMarkdownButton component.
+   */
   markdownPath?: string[]
+
+  /**
+   * Class name for the wrapper div element that organizes the layout of the breadcrumbs and the copy button.
+   */
   className?: string
 }
 
+/**
+ * Contains the breadcrumbs and the copy button, in a horizontal layout.
+ */
 export const PageHeaderWithCopyButton = ({
   config,
   markdownPath,
@@ -17,7 +31,6 @@ export const PageHeaderWithCopyButton = ({
   return (
     <div
       className={cn(
-        // Stack vertically on mobile (flex-col), horizontally on md+ (flex-row)
         'flex flex-col items-start md:flex-row justify-between mb-4 gap-2 md:gap-0',
         className,
       )}
