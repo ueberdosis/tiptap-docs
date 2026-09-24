00bbe3310d9461194a075d6f836ebeee87bc45e7
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2026-09-24 16:46:27 +0200

    docs: add new features to mobile navigation

--- src/components/MobileNavigationDropdown.tsx
+++ src/components/MobileNavigationDropdown.tsx
@@ -18,6 +18,8 @@ const options = [
         href: '/collaboration/getting-started/overview',
       },
       { label: 'Comments', href: '/comments/getting-started/overview' },
+      { label: 'Content Protection', href: '/content-protection/getting-started/overview' },
+      { label: 'Composable Documents', href: '/composable-documents/getting-started/overview' },
       {
         label: 'Tracked Changes',
         href: '/tracked-changes/getting-started/overview',
